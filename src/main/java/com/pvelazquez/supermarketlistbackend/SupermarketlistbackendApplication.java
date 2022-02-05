a098b2a39dd996bf26da50932573d8abe8230ac8
pvelazquez1096 <86578496+pvelazquez1096@users.noreply.github.com>
2022-02-04 19:14:41 -0600

    First Commit

---
+++ src/main/java/com/pvelazquez/supermarketlistbackend/SupermarketlistbackendApplication.java
@@ -0,0 +1,13 @@
+package com.pvelazquez.supermarketlistbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupermarketlistbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupermarketlistbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pvelazquez/supermarketlistbackend/SupermarketlistbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pvelazquez.supermarketlistbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketlistbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
